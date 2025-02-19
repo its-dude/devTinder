5425684e42124946a83e64483378e8e04924bed8
Abhishek Mishra <abhim5920@gmail.com>
2025-02-19 14:31:30 +0530

    created api to perform crud operation on db

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const  PORT=3000;
 
 app.use(express.json());
 
+//create user
 app.post('/signup',async(req,res)=>{
     try{
         const user=await User.create(req.body);
@@ -15,6 +16,51 @@ app.post('/signup',async(req,res)=>{
     }catch(err){
         console.log("error in creating user : ",err.message);
         res.status(501).send("failed to create user");
+    }
+})
+
+//find a user
+app.get('/user', async(req,res)=>{
+    try{
+        const user = await User.findOne({emailId:req.body.email});
+        res.send(user);
+    }catch(err){
+        console.log(err.message);
+        res.status(404).send("user doesn't exist");
+    }
+})
+
+//get all users
+app.get('/feed',async(req,res)=>{
+    try{
+        const users =await User.find({});
+        res.send(users);
+    }catch(err){
+        console.log(err.message);
+        res.status(404).send("No users");
+    }
+})
+
+//delelte a user
+app.delete('/user',async (req,res)=>{
+    try{
+        const userId = req.body.userid;
+        await User.findByIdAndDelete(userId);
+        res.send("user deleted successfully");
+    }catch{
+        res.status(404).send("User not found");
+    }
+})
+
+app.patch('/user',async(req,res)=>{
+    try{
+        const userid=req.body.userid;
+        const user=await User.findByIdAndUpdate(userid,req.body,{returnDocument:"before"});
+        console.log(user);
+        res.send("updated successfully");
+    }catch(err){
+        console.log(err.message);
+        res.status(404).send("failed to update");
     }
 })
 
--- src/config/mongoose_db.js
+++ src/config/mongoose_db.js
@@ -1,5 +1,10 @@
 import mongoose from "mongoose";
 const uri="mongodb+srv://abhishek10562:%40Backend2003@messanger-cluster.ngrf5.mongodb.net/devTinder" ;
-export const connecDB = async ()=>{
+const connecDB = async ()=>{
     await mongoose.connect(uri);
 }
+const disconnecDB=async()=>{
+    await mongoose.disconnect(uri);
+}
+
+export {connecDB,disconnecDB};
