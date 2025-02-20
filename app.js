83c207f21b02919105980dbe3305cf9c2ca9a002
Abhishek Mishra <abhim5920@gmail.com>
2025-02-20 10:57:26 +0530

    added validation

--- app.js
+++ app.js
@@ -7,15 +7,13 @@ const app=express();
 const  PORT=3000;
 
 app.use(express.json());
-
 //create user
 app.post('/signup',async(req,res)=>{
     try{
         const user=await User.create(req.body);
         res.send("User created !");
     }catch(err){
-        console.log("error in creating user : ",err.message);
-        res.status(501).send("failed to create user");
+        res.status(501).send("SIGNUP FAILED : "+err.message);
     }
 })
 
@@ -52,15 +50,18 @@ app.delete('/user',async (req,res)=>{
     }
 })
 
-app.patch('/user',async(req,res)=>{
+app.patch('/user/:userid',async(req,res)=>{
+    const userid=req.params?.userid;
+    const data = req.body;
     try{
-        const userid=req.body.userid;
-        const user=await User.findByIdAndUpdate(userid,req.body,{returnDocument:"before"});
-        console.log(user);
+        const  ALLOWED_UPDATES =["photoUrl","age","skills","gender","about"]
+        const isUpdateAllowed=Object.keys(data).every((key)=>ALLOWED_UPDATES.includes(key));
+        if(!isUpdateAllowed)throw new Error("updates not allowed");
+        const opts = { runValidators: true };
+        const user=await User.findByIdAndUpdate(userid,data,opts);
         res.send("updated successfully");
     }catch(err){
-        console.log(err.message);
-        res.status(404).send("failed to update");
+        res.status(404).send("UPDATE FAILED : "+err.message);
     }
 })
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,25 +1,67 @@
 import mongoose from "mongoose"
 const {Schema}= mongoose;
+import validator from "validator"
 
-const userSchema = new Schema({
+const userSchema = new Schema( {
     firstName:{
-        type:String
+        type:String,
+        required:true,
+        trim:true
     },
     lastName:{
         type:String
     },
     emailId:{
-        type:String
+        type:String,
+        unique:true,
+        trim:true,
+        required:true,
+        validate:{
+            validator:(value)=>/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value),
+            message:"Invalid email format"
+        }
     },
     gender:{
-        type:String
+        type:String,
+        validate:{
+            validator:(value)=>["male","female","others"].includes(value),
+            message:"Gender data is not valid"
+        }
     },
     age:{
-        type:Number
+        type:Number,
+        required:true,
+        min:[18,"Age must be at least 18"],
+        max:[100,"Age cannot exceed 100"],
+        validate:{
+            validator:Number.isInteger,
+            message:"Age must be a number"
+        }
     },
     password:{
-        type:String
+        type:String,
+        required:true,
+        validate:{
+            validator:(value)=>validator.isStrongPassword(value)
+        }        
+    },
+    photoUrl:{
+        type:String,
+        validate:{
+            validator:(value)=>validator.isURL(value)
+        }
+    },
+    skills:{
+        type:[String],
+        validate:{
+            validator:(value)=>value.length<=10,
+            message:"Skills can not be more than 10"
+        }
+    },
+    about:{
+        type:String,
+        maxlength:[200,"Only 200 characters are allowed"]
     }
-})
+},{timestamps:true})
 
 export const User = mongoose.model("User",userSchema);
