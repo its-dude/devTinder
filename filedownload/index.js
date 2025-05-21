329324531e0d73bc7b73661523cbad45a1d5c71e
Abhishek Mishra <abhim5920@gmail.com>
2025-05-21 14:29:35 +0530

    remove a folder from tracking

--- filedownload/index.js
+++
@@ -1,8 +0,0 @@
-const ProgressBar = require('progress')
-const bar = new ProgressBar(':bar', { total: 100 })
-const timer = setInterval(() => {
- bar.tick()
- if (bar.complete) {
- clearInterval(timer)
- }
-}, 100)
